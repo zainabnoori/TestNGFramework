947f5dcd474c2eaa70e8cf40e1905030d089d6f3
Your Name <you@example.com>
2019-06-09 11:52:39 -0400

    GitPracticeDemo class added to the project

---
+++ src/main/java/Framework/TestNGMaven/GitPracticeDemo1.java
@@ -0,0 +1,10 @@
+package Framework.TestNGMaven;
+
+public class GitPracticeDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is repository practice");
+	}
+
+}
